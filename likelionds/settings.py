b8db89ed58fd62fce842feece38b72bcc7ac1486
jiinkyung <jinkyung010310@gmail.com>
2023-02-18 03:39:40 +0900

    setting.py

--- likelionds/settings.py
+++ likelionds/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from distutils import config
 from pathlib import Path
 import os, json
 
@@ -91,8 +92,12 @@ WSGI_APPLICATION = 'likelionds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USERNAME"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT", cast=int),
     }
 }
 
