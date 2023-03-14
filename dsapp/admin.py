f1bd33adb310a31f5cbd1c9f59dd04f6b773c829
jiinkyung <jinkyung010310@gmail.com>
2023-03-15 01:22:18 +0900

    합불 명단 외 입력 오류 해결

--- dsapp/admin.py
+++ dsapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment, babyLion
+from .models import Post, Comment, babyLion, failure
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(babyLion)
+admin.site.register(failure)
